5187e439c3ca4cbe34e342f241452a68270e4278
T a m a r a <77888581+tami-pilla@users.noreply.github.com>
2023-04-21 10:21:53 -0300

    Add files via upload

--- restaurant query.sql
+++ restaurant query.sql
@@ -3,7 +3,7 @@ USE Restaurant
 SELECT * FROM dataset_restaurant_clean
 
 
----------------------------------------- exploratory data analisis ------------------------------------
+---------- exploratory data analisis --------------------
 
 
 
@@ -25,20 +25,22 @@ ORDER BY total_collected DESC, city
 
 
 
------------------------------------------------------------------------------------------
+------------------------------------------------------------
 
 --Restaurants
 
 
 -- Top 5 de restaurants con mayor ganancia
-SELECT TOP 5 restaurant_name, restaurant_type, SUM(total_orders) as total_money_collected
+SELECT TOP 5 restaurant_name, restaurant_type, 
+             SUM(total_orders) as total_money_collected
 FROM dataset_restaurant_clean
 GROUP BY  restaurant_name, restaurant_type
 ORDER BY total_money_collected DESC
 
 
 -- Top 5 de restaurants con mayor ganancia en tipo de comidas Vegana
-SELECT TOP 5 restaurant_name, restaurant_type, SUM(total_orders) as total_money_collected, meal_type
+SELECT TOP 5 restaurant_name, restaurant_type,
+             SUM(total_orders) as total_money_collected, meal_type
 FROM dataset_restaurant_clean
 WHERE meal_type = 'Vegan'
 GROUP BY  restaurant_name, restaurant_type, meal_type
@@ -47,7 +49,8 @@ ORDER BY total_money_collected DESC
 
 
 -- Top 5 de restaurants con mayor ganancia en tipo de restaurant Indio
-SELECT TOP 5 restaurant_name, restaurant_type, SUM(total_orders) as total_money_collected
+SELECT TOP 5 restaurant_name, restaurant_type, 
+           SUM(total_orders) as total_money_collected
 FROM dataset_restaurant_clean
 WHERE restaurant_type = 'Indian'
 GROUP BY  restaurant_name, restaurant_type
@@ -63,12 +66,11 @@ ORDER BY total_money_collected DESC
 
 --Miembros
 
-SELECT TOP 5  member_id, SUM(total_orders) as total_orders
+SELECT TOP 5  member_id, 
+            SUM(total_orders) as total_orders
 FROM dataset_restaurant_clean
 GROUP BY member_id
 ORDER BY total_orders DESC
-
-
 
 
 
@@ -94,62 +96,43 @@ WHERE hot_cold = 'Cold'
 
 
 
-
-
-
 -----------------------------------------------------------------------------------------
 
 
 --Fechas
 --desde 1/1/2020 - 1/7/2020
 
-
 SELECT * FROM dataset_restaurant_clean
 
-SELECT top 5 date, hour, MAX(total_orders) as max_total_orders
+SELECT top 5 date,
+             hour,
+			 MAX(total_orders) as max_total_orders
 FROM dataset_restaurant_clean
 GROUP BY date, hour, total_orders
 ORDER BY max_total_orders DESC
 
-SELECT FORMAT(date,'MM') as mes , COUNT(total_orders) as max_total_orders
-FROM dataset_restaurant_clean
-GROUP BY  total_orders, date
-ORDER BY date, COUNT(total_orders) DESC
-
-
-
-
-
------------------------------------------------------------------------------------------
-
-
-
-
-SELECT  date, COUNT(date) as catidad
-FROM dataset_restaurant_clean
-WHERE date = '2020-07-01'
-GROUP BY date
 
 SELECT FORMAT(date,'MM') as mes , 
        SUM(total_orders) as max_total_orders
 INTO obtener_mes
 FROM dataset_restaurant_clean
 GROUP BY date
-ORDER BY mes ASC
-
+ORDER BY date, SUM(total_orders) DESC
 
 SELECT * FROM obtener_mes
 
 
--- Total ganancia por mes
+-- Total gananciaS por mes
+
 SELECT mes as mes, 
        SUM(max_total_orders) as max_total_orders
+INTO ganancias_mes
 FROM obtener_mes
 GROUP BY mes
 ORDER BY max_total_orders DESC 
 
 
-
+SELECT * FROM ganancias_mes
 
 
 
